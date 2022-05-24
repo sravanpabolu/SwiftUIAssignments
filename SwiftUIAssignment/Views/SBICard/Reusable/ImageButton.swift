0775c090c685d86089181ce2967915bfb4a3e9dd
Sravan <sravanpabolu@gmail.com>
2022-05-24 13:14:09 +0530

    Buttons

---
+++ SwiftUIAssignment/Views/SBICard/Reusable/ImageButton.swift
@@ -0,0 +1,8 @@
+//
+//  ImageButton.swift
+//  SwiftUIAssignment
+//
+//  Created by Sravan Kumar Pabolu on 20/05/22.
+//
+
+import Foundation
---
+++ SwiftUIAssignment/Views/SBICard/YellowViews.swift
@@ -0,0 +1,20 @@
+//
+//  YellowViews.swift
+//  SwiftUIAssignment
+//
+//  Created by Sravan Kumar Pabolu on 20/05/22.
+//
+
+import SwiftUI
+
+struct YellowViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YellowViews_Previews: PreviewProvider {
+    static var previews: some View {
+        YellowViews()
+    }
+}
