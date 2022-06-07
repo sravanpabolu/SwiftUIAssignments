189463da5886e56fefa5daf8003f4c813c67004e
Sravan <sravanpabolu@gmail.com>
2022-06-08 01:27:53 +0530

    InterestCalculator utilities

--- SwiftUIAssignment/Reusable/Utils.swift
+++ SwiftUIAssignment/Reusable/Utils.swift
@@ -26,4 +26,30 @@ class Utils {
         dateFormatter.dateFormat = "MMMM dd"
         return dateFormatter.string(from: date)
     }
+    
+    static func offsetFrom(fromDate: Date, toDate: Date) -> String {
+        let dateComponentsFormatter = DateComponentsFormatter()
+        dateComponentsFormatter.allowedUnits = [.day, .month, .year]
+        dateComponentsFormatter.unitsStyle = .full
+
+        return dateComponentsFormatter.string(from: fromDate, to: toDate) ?? ""
+    }
+    
+    static func calculateInterest(for amount: Double, with interestRate: Double, fromDate: Date, toDate: Date) -> Double {
+        let duration = Utils.offsetFrom(fromDate: fromDate, toDate: toDate)
+        let shortDuration = duration.digits.condensed.trailingSpacesTrimmed
+        
+        let durationArr = Array(shortDuration.components(separatedBy: " ").reversed())
+
+        let days    = durationArr.count > 0 ? Double(durationArr[0]) : 0.0
+        let months  = durationArr.count > 1 ? Double(durationArr[1]) : 0.0
+        let years   = durationArr.count > 2 ? Double(durationArr[2]) : 0.0
+        
+        let interestYears = Double((amount * Double(years ?? 0) * interestRate) / 100)
+        let interestMonths = Double((amount * Double(months ?? 0) * interestRate) / (100 * 12))
+        let interestDays = Double((amount * Double(days ?? 0) * interestRate) / (100 * 12 * 30))
+        
+        return interestYears + interestMonths + interestDays
+    }
 }
+
